4fc309b3020791c67d8d74416f901f85cc484406
Proxypepe <sasha_golovin_95@bk.ru>
2023-07-23 18:37:35 +0300

    second version

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import typing
-
-import requests
-import json
 import dataclasses
 import pprint
+
+
+import requests
+
 
 USERNAME = 'admin'
 PASSWORD = 'password'
@@ -17,20 +17,23 @@ headers_base = {
 
 @dataclasses.dataclass
 class Repository:
+    """Repository entity"""
+
     name: str
     description: str
     repository_slug: str
     users: list[str] = dataclasses.field(default_factory=list)
     groups: list[str] = dataclasses.field(default_factory=list)
     validated: bool = False
-
-    def validate(self, rules: typing.Iterable[typing.Callable[[str], bool]]) -> bool:
-        # TODO добавить проверку на пустой проект, наличие всех необходимых групп, отсутствие лишних ролей
-        pass
+    res_description: str = ''
 
 
 @dataclasses.dataclass
 class Project:
+    """Project entity"""
+
+    # pylint: disable=too-many-instance-attributes
+
     name: str
     key: str
     description: str
@@ -38,79 +41,103 @@ class Project:
     users: list[str] = dataclasses.field(default_factory=list)
     repositories: list[Repository] = dataclasses.field(default_factory=list)
     validated: bool = False
-
-    def validate(self, rules: typing.Iterable[typing.Callable[[str], bool]]) -> bool:
-        pass
+    result_description: str = ''
 
 
 # Utility methods
-def make_request(url, headers, method='GET') -> requests.Response:
+def make_request(url, headers, method='GET', timeout=10) -> requests.Response:
     response = requests.request(
         method,
         url,
         headers=headers,
+        timeout=timeout,
         auth=(USERNAME, PASSWORD)
     )
     return response
 
 
 def create_rbac_project_rules(project_name: str) -> tuple[str, str, str]:
-    return 'rbac_bitbucket_read', f'rbac_bitbucket_{project_name}_admin', f'rbac_bitbucket_{project_name}_write'
+    return 'rbac_bitbucket_reader', \
+           f'rbac_bitbucket_{project_name}_admin'.replace(' ', '_').lower(), \
+           f'rbac_bitbucket_{project_name}_write'.replace(' ', '_').lower()
 
 
-def validate_units_rules(units: list[str], rules: tuple) -> bool:
-    # TODO Проверка на наличие всех необходимых групп, rules цепочка функторов func(unit: str) -> bool
-    for unit in units:
-        if unit not in rules:
-            return False
-    return True
+def is_contains_rbac_all_groups(groups: list[str], project_name: str) -> tuple[bool, str]:
+    rbac_groups = create_rbac_project_rules(project_name)
+    not_contains_groups = False
+    description = ''
+    for rbac_group in rbac_groups:
+        if rbac_group not in groups:
+            not_contains_groups = True
+            description += f'Отсутствует группа: {rbac_group} \n'
+    return not not_contains_groups, description
 
 
-def validate_groups(groups: list[str], rules: tuple) -> bool:
-    return validate_units_rules(groups, rules)
+def is_not_contains_extra_groups(groups: list[str], project_name: str) -> tuple[bool, str]:
+    rbac_groups = create_rbac_project_rules(project_name)
+    extra_group = False
+    description = ''
+    for group in groups:
+        if group not in rbac_groups:
+            extra_group = True
+            description += f'Лишняя группа: {group} \n'
+    return not extra_group, description
 
 
-def validate_users(users: list[str]) -> bool:
-    if not users:
-        return True
-    return all([user.startswith('srv') for user in users])
+def is_not_project_empty(repos_size: int) -> tuple[bool, str]:
+    return (False, 'Пустой репозиторий') if repos_size == 0 else (True, '')
+
+
+def validate_users(users: list[str]) -> tuple[bool, str]:
+    users_service_filtered = [user.startswith('srv') for user in users]
+    return (True, "") \
+        if all(users_service_filtered) \
+        else (False, 'есть лишние пользователи')
+
+
+def validate_all(*results) -> tuple[bool, str]:
+    boolean_values = [result[0] for result in results]
+    val = all(boolean_values)
+    description = [result[1] for result in results]
+    return val, ' '.join(description)
 
 
 # API methods
-def get_project_unit(unit: str, project_key: str) -> list[str]:
+def get_unit(url, unit: str):
     if unit not in ('users', 'groups'):
-        raise Exception('allowed units are users, groups')
+        raise ValueError('allowed units are users, groups')
     short_unit = unit[:-1]
-    url = f"{PROTOCOL}://{BASE_URL}/rest/api/latest/projects/{project_key}/permissions/{unit}"
     response = make_request(url, headers_base)
     values = response.json().get('values', [])
     project_unit = [value[short_unit]['name'] for value in values]
     return project_unit
 
 
+def get_project_unit(unit: str, project_key: str) -> list[str]:
+    url = f"{PROTOCOL}://{BASE_URL}/rest/api/latest/projects/{project_key}/permissions/{unit}"
+    return get_unit(url, unit)
+
+
 def get_repo_unit(unit: str, project_key: str, repository_slug: str) -> list[str]:
-    # TODO Избавиться от дублирования
-    if unit not in ('users', 'groups'):
-        raise Exception('allowed units are users, groups')
-    short_unit = unit[:-1]
     url = f"{PROTOCOL}://{BASE_URL}/rest/api/latest/projects/{project_key}/repos/{repository_slug}/permissions/{unit}"
-    response = make_request(url, headers_base)
-
-    values = response.json().get('values', [])
-    repo_unit = [value[short_unit]['name'] for value in values]
-    return repo_unit
+    return get_unit(url, unit)
 
 
+#####
 def get_repositories(project_key: str) -> list[Repository]:
     url = f"{PROTOCOL}://{BASE_URL}/rest/api/latest/projects/{project_key}/repos"
     response = make_request(url, headers_base)
     repositories = []
     for repository in response.json()['values']:
         description = repository.get('description', '')
-        repository_slug = repository['slug']
+        repository_slug = repository.get('slug')
         users = get_repo_unit('users', project_key, repository_slug)
         groups = get_repo_unit('groups', project_key, repository_slug)
-        validated = validate_users(users) & validate_groups(groups, ())
+
+        validated, res_description = validate_all(
+            validate_users(users),
+            (True, '') if not groups else (False, "Есть лишние группы")
+        )
         repositories.append(Repository(
             name=repository['name'],
             description=description,
@@ -118,12 +145,12 @@ def get_repositories(project_key: str) -> list[Repository]:
             users=users,
             groups=groups,
             validated=validated,
+            res_description=res_description,
         ))
     return repositories
 
 
 def audit() -> list[Project]:
-    # TODO Вынести логику запроса проектов
     limit = 30
     url = f"{PROTOCOL}://{BASE_URL}/rest/api/latest/projects?limit={limit}"
     response = make_request(url, headers_base)
@@ -131,19 +158,28 @@ def audit() -> list[Project]:
     projects = []
     for project in response.json()['values']:
         project_key = project['key']
+        project_name = project['name']
         groups = get_project_unit('groups', project['key'])
         users = get_project_unit('users', project['key'])
-        validated = validate_users(users) & validate_groups(groups, create_rbac_project_rules(project['name']))
         description = project.get('description', '')
         repositories = get_repositories(project_key)
+
+        validated, res_description = validate_all(
+            is_not_project_empty(len(repositories)),
+            is_not_contains_extra_groups(groups, project_name),
+            is_contains_rbac_all_groups(groups, project_name),
+            validate_users(users),
+        )
+
         projects.append(Project(
-            name=project['name'],
+            name=project_name,
             key=project_key,
             description=description,
             users=users,
             groups=groups,
             repositories=repositories,
-            validated=validated
+            validated=validated,
+            result_description=res_description
         ))
     return projects
 
